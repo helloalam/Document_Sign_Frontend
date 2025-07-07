5ea911a271f4b84722528db2b9f745e6416eaf29
helloalam <sahjadalam849@gmail.com>
2025-07-08 05:03:22 +0530

    update

--- src/pages/Home/pdfsign.jsx
+++ src/pages/Home/pdfsign.jsx
@@ -146,8 +146,8 @@ export default function PdfViewer() {
       <div
         ref={containerRef}
         className="relative border bg-white shadow-md overflow-auto w-full lg:w-2/3"
-        style={{ minHeight: "90vh" }}
-      >
+         style={{ minHeight: "90vh" }}> // or 60vh if preferred
+
         <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
           <Page
             pageNumber={page}
